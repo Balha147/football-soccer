aa4911f0c2dc5a02f51dcf867f5b47acf9501af1
unknown <baslymohamedhedi@gmail.com>
2023-11-21 19:29:06 +0100

    refacto

--- src/app/football-soccer/components/menu/menu.component.ts
+++ src/app/football-soccer/components/menu/menu.component.ts
@@ -2,26 +2,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { NgClass, NgFor } from '@angular/common';
 import { LEAGUE_HEADER_ITEMS } from '../../config/league-items.config';
 import { LeagueItemModel } from '../../models/league-item.model';
-import { ClickHighlightDirective } from '../../directive/click-highlight.directive';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [NgFor, ClickHighlightDirective, NgClass],
+  imports: [NgFor, NgClass],
 })
 export class MenuComponent {
   leagueItems = LEAGUE_HEADER_ITEMS;
+
+  selectedLeagueId: number = this.leagueItems[0].id
 
   @Output()
   onReadySelectCountry: EventEmitter<number> = new EventEmitter<number>();
 
   onClickCountry(selectLeague: LeagueItemModel): void { // id: number router active route
-    // a supprimer
-    // this.leagueItems.forEach((item: LeagueItemModel) => {
-    //   item.isSelected = (item === selectLeague);
-    // });
+
+    this.selectedLeagueId = selectLeague.id;
     this.onReadySelectCountry.emit(selectLeague.id);
   }
 
